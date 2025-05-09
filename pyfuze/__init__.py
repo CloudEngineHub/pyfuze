be43ffd3497d6b0babe2bddbef2a4f49e6979b1f
Tanix <tanixlu@qq.com>
2025-05-10 06:13:22 +0800

    use typer

--- pyfuze/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-import subprocess
-
-
-def run_pyfuze(*args):
-    binary = os.path.join(os.path.dirname(__file__), "pyfuze.com")
-    return subprocess.run([binary] + list(args))
--- pyfuze/cli.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-from . import run_pyfuze
-
-
-def main():
-    sys.exit(run_pyfuze(*sys.argv[1:]).returncode)
